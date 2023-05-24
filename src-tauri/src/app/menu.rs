819b158426e1915dd431d84cc1788aa2dc15694d
jeffzwang <jeffreyzhiyuanwang@gmail.com>
2023-05-23 18:41:04 -0700

    add removal of stay on top

--- src-tauri/src/app/menu.rs
+++ src-tauri/src/app/menu.rs
@@ -34,7 +34,7 @@ pub fn init() -> Menu {
   );
 
   let stay_on_top =
-    CustomMenuItem::new("stay_on_top".to_string(), "Stay On Top").accelerator("CmdOrCtrl+T");
+    CustomMenuItem::new("stay_on_top".to_string(), "Stay On Top");
   let stay_on_top_menu = if app_conf.stay_on_top {
     stay_on_top.selected()
   } else {
